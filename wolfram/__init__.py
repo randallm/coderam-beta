49c61ecf348e128fd95a1cf7efdb625ec7589b45
Randall Ma <randall@randallma.com>
2013-02-17 05:28:04 -0800

    styling updates

--- wolfram/__init__.py
+++ wolfram/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('wolfram.config')
 app.jinja_env.globals.update(zip=zip)
+app.jinja_env.globals.update(bool=bool)
 
 import wolfram.views
